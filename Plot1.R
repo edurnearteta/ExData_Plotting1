0cdfb1c14d8ce722a6ce93a1abf1d4a23dd7215e
edurnearteta <edurne.arteta@gmail.com>
2014-05-11 17:30:02 +0200

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,27 @@
+# Plot1.R - Histogram of Global Active Power
+
+# Read data
+data <- read.table("household_power_consumption.txt", sep=";", header=T,
+                   colClasses = c("character", "character", "numeric",
+                                  "numeric", "numeric", "numeric",
+                                  "numeric", "numeric", "numeric"),
+                   na.strings="?")
+
+# Convert dates
+data$DateTime <- strptime(paste(data$Date, data$Time),
+                          "%d/%m/%Y %H:%M:%S")
+# Subset to the dates 2007-02-01 and 2007-02-02
+data <- subset(data,
+       as.Date(DateTime) >= as.Date("2007-02-01") &
+           as.Date(DateTime) <= as.Date("2007-02-02"))
+
+# Plot1.png
+png("Plot1.png", height=480, width=480)
+
+# Histogram
+hist(data$Global_active_power, col='red',
+     xlab = 'Global Active Power (kilowatts)',
+     main = 'Global Active Power')
+
+# Close PNG
+dev.off()
